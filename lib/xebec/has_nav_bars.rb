db8a0d490fff81ee3fcb9a733a177af3502890f2
James A. Rosen <james.a.rosen@gmail.com>
2010-03-03 17:46:51 -0500

    refactored NavBar lookup from NavBarHelper into a HasNavBars mixin in preparation for ControllerSupport

---
+++ lib/xebec/has_nav_bars.rb
@@ -0,0 +1,29 @@
+module Xebec
+  
+  # A supporting mixin for NavBarHelper and ControllerSupport.
+  # Looks up navigation bars by name.
+  module HasNavBars
+    
+    protected
+    
+    # Looks up the named nav bar, creates it if it
+    # doesn't exist, and evaluates the the block, if
+    # given, in the scope of the nav bar.
+    def look_up_nav_bar_and_eval(name = nil, &block)
+      name ||= Xebec::NavBar::DEFAULT_NAME
+      look_up_nav_bar(name).tap do |bar|
+        bar.instance_eval &block if block_given?
+      end
+    end
+    
+    def look_up_nav_bar(name)
+      nav_bars[name] ||= NavBar.new(name)
+    end
+    
+    def nav_bars
+      @nav_bars ||= {}
+    end
+    
+  end
+  
+end
--- lib/xebec/nav_bar_helper.rb
+++ lib/xebec/nav_bar_helper.rb
@@ -1,6 +1,10 @@
+require 'xebec/has_nav_bars'
+
 module Xebec
   
   module NavBarHelper
+    
+    include Xebec::HasNavBars
     
     # If called in an output expression ("<%= navbar %>" in ERB
     # or "=navbar" in HAML), renders the navigation bar.
@@ -23,29 +27,29 @@ module Xebec
     #   end %>
     #
     # @see Xebec::NavBar#nav_item
+    # @see Xebec::HasNavBars#nav_bar
     def nav_bar(name = nil, &block)
-      look_up_nav_bar(name).tap do |bar|
-        bar.instance_eval &block if block_given?
-      end
+      look_up_nav_bar_and_eval name, &block
     end
     
     # Renders a navigation bar if and only if it contains any
     # navigation items. Unlike +nav_bar+, this method does not
     # accept a block.
     def nav_bar_unless_empty(name = nil)
-      bar = look_up_nav_bar(name)
+      bar = look_up_nav_bar name
       bar.empty? ? '' : bar
     end
     
     protected
     
-    def look_up_nav_bar(name = nil)
-      name ||= Xebec::NavBar::DEFAULT_NAME
-      nav_bars[name] ||= NavBarProxy.new(NavBar.new(name), self)
-    end
-    
-    def nav_bars
-      @nav_bars ||= {}
+    # Override HasNavBars#look_up_nav_bar to replace with a
+    # proxy if necessary.
+    def look_up_nav_bar(name)
+      bar = super(name)
+      if bar.kind_of?(Xebec::NavBar)
+        bar = nav_bars[bar.name] = NavBarProxy.new(bar, self)
+      end
+      bar
     end
     
   end
