2c652238ddbadd8e72a78557ea39084ec8934e50
James A. Rosen <james.a.rosen@gmail.com>
2010-03-04 18:15:06 -0500

    fixed example application ProjectsController: was calling selected= instead of current= on NavBars

--- doc/example_app/app/controllers/projects_controller.rb
+++ doc/example_app/app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   
   # set the selected item for the :area navigation bar
   # for all actions in this controller:
-  nav_bar :area { |nb| nb.selected = :projects }
+  nav_bar(:area) { |nb| nb.current = :projects }
   
   def index
     params[:sort] ||= 'recent'
@@ -10,7 +10,7 @@ class ProjectsController < ApplicationController
     nav_bar(:tabs) do |nb|
       nb.nav_item :by_alpha, projects_path(:sort => 'by_alpha')
       nb.nav_item :recent,   projects_path(:sort => 'recent')
-      nb.selected = params[:sort]
+      nb.current = params[:sort]
     end
     @projects = Project.ordered_by(params[:sort])
   end
@@ -42,7 +42,7 @@ class ProjectsController < ApplicationController
       nb.nav_item :budget,   budget_project_path(@project)
       nb.nav_item :history,  history_project_path(@project)
       nb.nav_item :edit,     edit_project_path(@project)
-      nb.selected = selected
+      nb.current = selected
     end
   end
   
