fb4f40855da6fac036b69aecd5a08f51a1942558
Nat Budin <natbudin@gmail.com>
2010-04-12 12:43:21 -0400

    Add extra 'options' parameter to all nav bar lookup methods, and using it, allow a custom renderer to be passed (fixes #1)

--- lib/xebec/has_nav_bars.rb
+++ lib/xebec/has_nav_bars.rb
@@ -15,14 +15,14 @@ module Xebec
     # @param [Symbol, String] name the name of the navigation bar to look up
     # @param [Hash] html_attributes additional HTML attributes to add to the
     #                               navigation bar
-    def look_up_nav_bar_and_eval(name = nil, html_attributes = {}, &block)
+    def look_up_nav_bar_and_eval(name = nil, html_attributes = {}, options = {}, &block)
       name ||= Xebec::NavBar::DEFAULT_NAME
-      look_up_nav_bar(name, html_attributes).tap do |bar|
+      look_up_nav_bar(name, html_attributes, options).tap do |bar|
         block.bind(self).call(bar) if block_given?
       end
     end
     
-    def look_up_nav_bar(name, html_attributes)
+    def look_up_nav_bar(name, html_attributes, options = {})
       (nav_bars[name] ||= NavBar.new(name, html_attributes)).tap do |bar|
         bar.html_attributes.merge!(html_attributes)
       end
